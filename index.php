cc0ab969d7365f3d5e8a75b683da5223ce58fcbd
Najmul Huda <najmul.myself@gmail.com>
2021-10-03 22:40:45 +0600

    html php integration

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 
-echo "Hello World <br/>";
+// echo "Hello World <br/>";
 
-$a = 2;
-$b = 5;
-$total = $a * $b;
+// $a = 2;
+// $b = 5;
+// $total = $a * $b;
 
-echo "\n {$total} = {$a} * {$b}";
+// echo "\n {$total} = {$a} * {$b}";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Simple Project</title>
+</head>
+<body>
+    <?php
+    echo "<h1>PHP Simple Project</h1>";
+    ?>
+</body>
+</html>
